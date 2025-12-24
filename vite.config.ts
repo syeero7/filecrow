4fd22bbb10934d4a5694ea27aff8afa6f18e7d0e
emal <emalhasitha@proton.me>
2025-12-25 00:20:34 +0530

    build: uninstall tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 const PORT = 8080;
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [tailwindcss(), ViteMinifyPlugin()],
+    plugins: [ViteMinifyPlugin()],
     ...(mode.startsWith("dev") && {
       server: {
         proxy: {
