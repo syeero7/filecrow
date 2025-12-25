4db2c63e1dbe1e35a8e941b31850cdf53e2b5e31
emal <emalhasitha@proton.me>
2025-12-25 23:39:07 +0530

    fix: extend server shutdown timeout to 10 seconds

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 	}
 
 	log.Println("server is shutting down...")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
